1c8536a9bc2bb87f8db11e99b786c2f7a11586d2
Naresh Kumar <Naresh Kumar@DESKTOP-936Q59C>
2025-04-15 18:09:37 +0530

    Code changes done

---
+++ AutomationFramework/src/test/java/digitaldownloads/TC_DWS_003_Test.java
@@ -0,0 +1,13 @@
+package digitaldownloads;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_DWS_003_Test {
+
+	@Test
+	public void main()
+	{
+		Reporter.log("Code changes done");
+	}
+}
